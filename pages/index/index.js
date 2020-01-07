bffba00d75c4fed41a52d97e2ba69a8866903efd
Stephen <1137657002@qq.com>
2020-01-07 18:14:32 +0800

    fix a bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,33 @@ Page({
     videoUrl: '',
     defaultUrl: 'http://v.douyin.com/aWcudQ/'
   },
+  onLoad: function (e) {
+    wx.showShareMenu({
+      // 要求小程序返回分享目标信息
+      withShareTicket: true
+    });
+  },
+  /* 转发*/
+  onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+    }
+    return {
+      title: '转发dom',
+      path: `pages/index/index`,
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功:" + JSON.stringify(res));
+        var shareTickets = res.shareTickets;
+
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
+  },
   //事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
